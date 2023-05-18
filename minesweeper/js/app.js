6ed6e2c53e72c1b74a9d8c7dfc85daac0d0d6d75
vitali007tut <vitali007@tut.by>
2023-05-18 23:14:23 +0300

    feat: add check for Win

--- minesweeper/js/app.js
+++ minesweeper/js/app.js
@@ -5,26 +5,17 @@ document.head.insertAdjacentHTML(
 );
 
 let sec = 0;
-const result = document.createElement("div");
-result.classList.add("result");
-document.body.appendChild(result);
-
-function showTimer(seconds) {
-  timer.innerHTML = `${Math.floor(seconds / 60)} min ${seconds % 60} sec`;
-}
-
 let time = null;
-function startTimer() {
-  time = setInterval(() => {
-    sec++;
-    showTimer(sec);
-  }, 1000);
-}
-
+let squares = [];
+// let stepsNumber = 0;
 const width = 10;
 const minesAmount = 10;
 let flags = 0;
 let isGameOver = false;
+
+const result = document.createElement("div");
+result.classList.add("result");
+document.body.appendChild(result);
 
 const container = document.createElement("div");
 container.classList.add("container");
@@ -45,32 +36,38 @@ const flagsAmount = document.querySelector(".flags-amount");
 
 const btnStart = document.createElement("button");
 btnStart.classList.add(".btn-start");
-btnStart.innerHTML = "New g";
+btnStart.innerHTML = "New game";
 scoreBar.appendChild(btnStart);
 document.querySelector(".btn-start");
-// new game using button click
-btnStart.addEventListener("click", () => { 
-  flags = 0;
-  isGameOver = false;
-  result.innerHTML = '';
-  mineContentInner.innerHTML = '';
-  createBoard();
-  clearInterval(time);
-  time = null;
-  sec = 0;
-  showTimer(sec);
-});
 
 const timer = document.createElement("div");
 timer.classList.add("timer");
-timer.innerHTML = `${sec / 60} min ${sec % 60} sec`;
+timer.innerHTML = `${sec % 60} sec`;
 scoreBar.appendChild(timer);
 
 const mineContentInner = document.createElement("div");
 mineContentInner.classList.add("mine-content__inner");
 mineContent.appendChild(mineContentInner);
 
-let squares = [];
+function startTimer() {
+  time = setInterval(() => {
+    sec++;
+    timer.innerHTML = `${sec % 60} sec`;
+  }, 1000);
+}
+
+// new game using button click
+// btnStart.addEventListener("click", () => {
+//   flags = 0;
+//   isGameOver = false;
+//   result.innerHTML = '';
+//   mineContentInner.innerHTML = '';
+//   createBoard();
+//   clearInterval(time);
+//   time = null;
+//   sec = 0;
+//   showTimer(sec);
+// });
 
 function createBoard() {
   flagsAmount.innerHTML = minesAmount;
@@ -79,14 +76,14 @@ function createBoard() {
   const emptyArray = Array(width * width - minesAmount).fill("empty");
   const gameArray = emptyArray.concat(minesArray);
   const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
-  
+
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
     square.classList.add(`ceil`, `${shuffledArray[i]}`);
     mineContentInner.appendChild(square);
     squares.push(square);
-  
+
     square.addEventListener("click", function (e) {
       click(square);
     });
@@ -138,12 +135,13 @@ function createBoard() {
     }
   }
 }
+
 createBoard();
-
-
 
 function click(square) {
   // console.log("ceil is", square.classList[1]);
+  let currentId = square.id;
+  if (isGameOver) return;
 
   if (
     document.querySelectorAll(".checked").length === 0 &&
@@ -153,20 +151,18 @@ function click(square) {
       console.log("game started");
       startTimer();
     } else {
-      console.log(square.getAttribute('id'))
+      console.log(square.getAttribute("id"));
       // const newSquareId = square.getAttribute('id')
-      mineContentInner.innerHTML = ''
+      mineContentInner.innerHTML = "";
       createBoard();
       // console.log(square.getAttribute('id'))
-      console.log(document.getElementById(square.getAttribute('id')))
-      const newSquare = document.getElementById(square.getAttribute('id'))
+      console.log(document.getElementById(square.getAttribute("id")));
+      const newSquare = document.getElementById(square.getAttribute("id"));
       click(newSquare);
-      return
+      return;
     }
   }
 
-  let currentId = square.id;
-  if (isGameOver) return;
   if (square.classList.contains("checked") || square.classList.contains("flag"))
     return;
   if (square.classList.contains("mine")) {
@@ -175,26 +171,28 @@ function click(square) {
     let total = square.getAttribute("data");
     if (total != 0) {
       square.classList.add("checked");
-      if (total == 1) square.classList.add("one");
-      if (total == 2) square.classList.add("two");
-      if (total == 3) square.classList.add("three");
-      if (total == 4) square.classList.add("four");
-      if (total == 5) square.classList.add("five");
-      if (total == 6) square.classList.add("six");
+      if (total === "1") square.classList.add("one");
+      if (total === "2") square.classList.add("two");
+      if (total === "3") square.classList.add("three");
+      if (total === "4") square.classList.add("four");
+      if (total === "5") square.classList.add("five");
+      if (total === "6") square.classList.add("six");
       square.innerHTML = total;
       return;
     }
-    checkSquare(square, currentId);
+    checkSquare(square, currentId); // if total = 0, recursion on id
   }
   square.classList.add("checked");
 }
 
 function addFlag(square) {
+  if (isGameOver) return;
   if (!square.classList.contains("flag")) {
     square.classList.add("flag");
     square.innerHTML = " 🚩";
     flags++;
     flagsAmount.innerHTML = minesAmount - flags;
+    checkForWin();
   } else {
     square.classList.remove("flag");
     square.innerHTML = "";
@@ -205,7 +203,8 @@ function addFlag(square) {
 
 function gameOver(square) {
   clearInterval(time);
-  result.innerHTML = "BOOM! Game Over! Try one more time...";
+  result.innerHTML = "Game Over! Try one more time...";
+  result.classList.add("game-over");
   isGameOver = true;
 
   squares.forEach((square) => {
@@ -218,57 +217,66 @@ function gameOver(square) {
 }
 
 function checkSquare(square, currentId) {
-  const isLeftEdge = currentId % width === 0;
-  const isRightEdge = currentId % width === width - 1;
-
+  const isLeftEdge = currentId % width === 0; // left column
+  const isRightEdge = currentId % width === width - 1; // right column
   setTimeout(() => {
     if (currentId > 0 && !isLeftEdge) {
       const newId = squares[parseInt(currentId) - 1].id;
-      //const newId = parseInt(currentId) - 1   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId > 9 && !isRightEdge) {
       const newId = squares[parseInt(currentId) + 1 - width].id;
-      //const newId = parseInt(currentId) +1 -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId > 10) {
       const newId = squares[parseInt(currentId - width)].id;
-      //const newId = parseInt(currentId) -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId > 11 && !isLeftEdge) {
+    if (currentId > 10 && !isLeftEdge) {
       const newId = squares[parseInt(currentId) - 1 - width].id;
-      //const newId = parseInt(currentId) -1 -width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId < 98 && !isRightEdge) {
+    if (currentId < 99 && !isRightEdge) {
       const newId = squares[parseInt(currentId) + 1].id;
-      //const newId = parseInt(currentId) +1   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 90 && !isLeftEdge) {
       const newId = squares[parseInt(currentId) - 1 + width].id;
-      //const newId = parseInt(currentId) -1 +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
     if (currentId < 88 && !isRightEdge) {
       const newId = squares[parseInt(currentId) + 1 + width].id;
-      //const newId = parseInt(currentId) +1 +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId < 89) {
+    if (currentId < 90) {
       const newId = squares[parseInt(currentId) + width].id;
-      //const newId = parseInt(currentId) +width   ....refactor
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
   }, 10);
+}
+
+function checkForWin() {
+  let matches = 0;
+  for (let i = 0; i < squares.length; i++) {
+    if (
+      squares[i].classList.contains("flag") &&
+      squares[i].classList.contains("mine")
+    ) {
+      matches++;
+    }
+    if (matches === minesAmount) {
+      result.innerHTML = `Hooray! You found all mines in ${sec} seconds and N moves`;
+      result.classList.add('game-win')
+      clearInterval(time);
+      isGameOver = true;
+    }
+  }
 }
