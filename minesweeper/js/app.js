542e97d50da08ec932a8a870c7e8d7ba61c57c1e
vitali007tut <vitali007@tut.by>
2023-05-21 19:11:58 +0300

    feat: implement ability to change the size

--- minesweeper/js/app.js
+++ minesweeper/js/app.js
@@ -8,15 +8,15 @@ let sec = 0;
 let time = null;
 let squares = [];
 let stepsNumber = 0;
-const width = 10;
-const minesAmount = 10;
 let flags = 0;
 let isGameOver = false;
+if (!localStorage.getItem('size')) localStorage.setItem('size', 'easy');
+const size = localStorage.getItem('size');
 
 // add in body
 const result = addHtmlElement("div", 'result', document.body);
 const container = addHtmlElement("div", 'container', document.body);
-// add in contaner
+// add in container
 const dropDowns = addHtmlElement('div', 'mine__drop-downs', container);
 const topBar = addHtmlElement('div', 'mine__top-bar', container);
 // add in dropDowns
@@ -26,15 +26,19 @@ dropDown.classList.add('hidden');
 const dropDownTitle = addHtmlElement('div', 'mine__drop-down__title', dropDown, 'Game settings')
 dropDownTitle.addEventListener('click', () => {
   dropDown.classList.toggle('hidden');
+  dropDowns.classList.toggle('z-index');
+  backOpacity.classList.toggle('menu-open');
 })
 const dropDownMenu = addHtmlElement('div', 'mine__drop-down__menu', dropDown);
 // add in dropDownMenu
 const dropDownRow = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
 // add in dropDownRow
-addHtmlElement('div', 'mine__drop-down__check', dropDownRow);
+// addHtmlElement('div', 'mine__drop-down__check', dropDownRow);
 const dropDownText = addHtmlElement('div', 'mine__drop-down__text', dropDownRow, 'New game');
 dropDownText.addEventListener("click", () => {
   dropDown.classList.toggle('hidden');
+  dropDowns.classList.toggle('z-index');
+  backOpacity.classList.toggle('menu-open');
   clearBoard()
   createBoard();
   if (sound.checked) new Audio('./asserts/start.wav').play();
@@ -43,20 +47,76 @@ dropDownText.addEventListener("click", () => {
 addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
 const dropDownRowSound = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
 // add in dropDownSound
-addHtmlElement('div', 'mine__drop-down__check', dropDownRowSound);
+// addHtmlElement('div', 'mine__drop-down__check', dropDownRowSound);
 const dropDownTextSound = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSound,
   `<label><input type="checkbox" id="sound">Sound
   </label>`);
 const sound = document.querySelector('#sound')
 sound.addEventListener('change', () => {
-  dropDown.classList.toggle('hidden');
   localStorage.setItem('sound', sound.checked);
 })
+// add separator in dropDownMenu after Sound
+addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
+// add size radio in dropDownMenu
+const dropDownRowSize1 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+const dropDownTextSize1 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize1,
+`<label><input type="radio" id="easy" name="size" value="easy" checked>Easy (10x10)</label>`)
+const dropDownRowSize2 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+const dropDownTextSize2 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize2,
+`<label><input type="radio" id="medium" name="size" value="medium">Medium (15x15)</label>`)
+const dropDownRowSize3 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+const dropDownTextSize3 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize3,
+  `<label><input type="radio" id="hard" name="size" value="hard">Hard (25x25)</label>`);
+// const size = document.querySelector('gg');
+document.querySelectorAll('input[name="size"]').forEach((e) => {
+  e.addEventListener('change', () => {
+    localStorage.setItem('size', e.value)
+    clearBoard();
+    createBoard();
+  })
+})
+
+const sizeObject = {
+  easy: {
+    widthContent: '266px', // mineContent.style.width
+    grid: 'repeat(10, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthCeil: 10,
+    amountMines: 10,
+  },
+  medium: {
+    widthContent: '390px', // mineContent.style.width
+    grid: 'repeat(15, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthCeil: 15,
+    amountMines: 40,
+  },
+  hard: {
+    widthContent: '640px', // mineContent.style.width
+    grid: 'repeat(25, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthCeil: 25,
+    amountMines: 99,
+  },
+}
+
+// add separator in dropDownMenu after Size
+addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
+// add last results in dropDownMenu
+const dropDownRowResults = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+const dropDownResults = addHtmlElement('div', 'mine__drop-down__text', dropDownRowResults, 'Last 10 results...');
+
 
 // add in topBar
+const backOpacity = addHtmlElement('div', 'backOpacity', topBar); 
 const topBarText = addHtmlElement('div', 'mine__top-bar__text', topBar, 'Game settings')
 topBarText.addEventListener('click', () => {
   dropDown.classList.toggle('hidden');
+  dropDowns.classList.toggle('z-index');
+  backOpacity.classList.toggle('menu-open');
+})
+
+backOpacity.addEventListener('click', () => {
+  dropDown.classList.toggle('hidden');
+  dropDowns.classList.toggle('z-index');
+  backOpacity.classList.toggle('menu-open');
 })
 
 function addHtmlElement(element, classElement, insert, innerText = '') {
@@ -66,13 +126,8 @@ function addHtmlElement(element, classElement, insert, innerText = '') {
   return insert.appendChild(newElement)
 }
 
-const mineContent = document.createElement("div");
-mineContent.classList.add("mine-content");
-container.appendChild(mineContent);
-
-const scoreBar = document.createElement("div");
-scoreBar.classList.add("mine__score-bar");
-mineContent.appendChild(scoreBar);
+const mineContent = addHtmlElement('div', 'mine-content', container);
+const scoreBar = addHtmlElement('div', 'mine__score-bar', mineContent);
 
 const flagsLine = document.createElement("div");
 flagsLine.innerHTML = `Flags left: <span class='flags-amount'></span>`;
@@ -93,6 +148,9 @@ const mineContentInner = document.createElement("div");
 mineContentInner.classList.add("mine-content__inner");
 mineContent.appendChild(mineContentInner);
 
+// add radio mark if reload window
+document.getElementById(size).checked = true;
+
 function startTimer() {
   time = setInterval(() => {
     sec++;
@@ -101,12 +159,16 @@ function startTimer() {
 }
 
 function createBoard() {
-  flagsAmount.innerHTML = minesAmount;
-
-  // if (localStorage.getItem('sound') === 'false') {
-  //   sound.checked = false;
-  // } else sound.checked = true;
   sound.checked = localStorage.getItem('sound') === 'true' ? true : false;
+  // check size in localStorage
+  const size = localStorage.getItem('size');
+  const width = sizeObject[size].widthCeil;
+  const minesAmount = sizeObject[size].amountMines;
+  flagsAmount.innerHTML = minesAmount;
+  // check styles in localStorage
+  mineContent.style.width = sizeObject[size].widthContent;
+  mineContentInner.style.gridTemplateColumns = sizeObject[size].grid;
+  mineContentInner.style.gridTemplateRows = sizeObject[size].grid;
 
   const minesArray = Array(minesAmount).fill("mine");
   const emptyArray = Array(width * width - minesAmount).fill("empty");
@@ -211,6 +273,9 @@ function click(square) {
 }
 
 function addFlag(square) {
+  const size = localStorage.getItem('size');
+  const minesAmount = sizeObject[size].amountMines;
+
   if (isGameOver) return;
   if (!square.classList.contains("flag")) {
     square.classList.add("flag");
@@ -243,6 +308,9 @@ function gameOver(square) {
 }
 
 function checkSquare(square, currentId) {
+  const size = localStorage.getItem('size');
+  const width = sizeObject[size].widthCeil;
+
   const isLeftEdge = currentId % width === 0; // left column
   const isRightEdge = currentId % width === width - 1; // right column
   setTimeout(() => {
@@ -290,6 +358,9 @@ function checkSquare(square, currentId) {
 }
 
 function checkForWin() {
+  const size = localStorage.getItem('size');
+  const minesAmount = sizeObject[size].amountMines;
+
   let matches = 0;
   for (let i = 0; i < squares.length; i++) {
     if (
@@ -309,6 +380,9 @@ function checkForWin() {
 }
 
 function clearBoard() {
+  const size = localStorage.getItem('size');
+  const minesAmount = sizeObject[size].amountMines;
+
   mineContentInner.innerHTML = '';
   flags = 0;
   clearInterval(time);
