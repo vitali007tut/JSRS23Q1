4729fb813c756ca752f142f59d48c02e7ce94c6c
vitali007tut <vitali007@tut.by>
2023-05-22 10:57:34 +0300

    feat: create last 10 results

--- minesweeper/js/app.js
+++ minesweeper/js/app.js
@@ -10,124 +10,177 @@ let squares = [];
 let stepsNumber = 0;
 let flags = 0;
 let isGameOver = false;
-if (!localStorage.getItem('size')) localStorage.setItem('size', 'easy');
-const size = localStorage.getItem('size');
+if (!localStorage.getItem("size")) localStorage.setItem("size", "easy");
+const size = localStorage.getItem("size");
+
+
+if (!localStorage.getItem("Results")) localStorage.setItem("Results", []);
+const resultsArray = localStorage.getItem("Results").split(',');
 
 // add in body
-const result = addHtmlElement("div", 'result', document.body);
-const container = addHtmlElement("div", 'container', document.body);
+const result = addHtmlElement("div", "result", document.body);
+const container = addHtmlElement("div", "container", document.body);
 // add in container
-const dropDowns = addHtmlElement('div', 'mine__drop-downs', container);
-const topBar = addHtmlElement('div', 'mine__top-bar', container);
+const dropDowns = addHtmlElement("div", "mine__drop-downs", container);
+const topBar = addHtmlElement("div", "mine__top-bar", container);
 // add in dropDowns
-const dropDown = addHtmlElement('div', 'mine__drop-down', dropDowns);
-dropDown.classList.add('hidden');
+const dropDown = addHtmlElement("div", "mine__drop-down", dropDowns);
+dropDown.classList.add("hidden");
 // add in dropDown
-const dropDownTitle = addHtmlElement('div', 'mine__drop-down__title', dropDown, 'Game settings')
-dropDownTitle.addEventListener('click', () => {
-  dropDown.classList.toggle('hidden');
-  dropDowns.classList.toggle('z-index');
-  backOpacity.classList.toggle('menu-open');
-})
-const dropDownMenu = addHtmlElement('div', 'mine__drop-down__menu', dropDown);
+const dropDownTitle = addHtmlElement(
+  "div",
+  "mine__drop-down__title",
+  dropDown,
+  "Game settings"
+);
+dropDownTitle.addEventListener("click", () => toggleMenu());
+const dropDownMenu = addHtmlElement("div", "mine__drop-down__menu", dropDown);
 // add in dropDownMenu
-const dropDownRow = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+const dropDownRow = addHtmlElement("div", "mine__drop-down__row", dropDownMenu);
 // add in dropDownRow
 // addHtmlElement('div', 'mine__drop-down__check', dropDownRow);
-const dropDownText = addHtmlElement('div', 'mine__drop-down__text', dropDownRow, 'New game');
+const dropDownText = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRow,
+  "New game"
+);
 dropDownText.addEventListener("click", () => {
-  dropDown.classList.toggle('hidden');
-  dropDowns.classList.toggle('z-index');
-  backOpacity.classList.toggle('menu-open');
-  clearBoard()
+  toggleMenu();
+  clearBoard();
   createBoard();
-  if (sound.checked) new Audio('./asserts/start.wav').play();
+  if (sound.checked) new Audio("./asserts/start.wav").play();
 });
 // add separator in dropDownMenu
-addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
-const dropDownRowSound = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
+addHtmlElement("div", "mine__drop-down__separator", dropDownMenu);
+const dropDownRowSound = addHtmlElement(
+  "div",
+  "mine__drop-down__row",
+  dropDownMenu
+);
 // add in dropDownSound
 // addHtmlElement('div', 'mine__drop-down__check', dropDownRowSound);
-const dropDownTextSound = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSound,
+const dropDownTextSound = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRowSound,
   `<label><input type="checkbox" id="sound">Sound
-  </label>`);
-const sound = document.querySelector('#sound')
-sound.addEventListener('change', () => {
-  localStorage.setItem('sound', sound.checked);
-})
+  </label>`
+);
+const sound = document.querySelector("#sound");
+sound.addEventListener("change", () => {
+  localStorage.setItem("sound", sound.checked);
+});
 // add separator in dropDownMenu after Sound
-addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
+addHtmlElement("div", "mine__drop-down__separator", dropDownMenu);
 // add size radio in dropDownMenu
-const dropDownRowSize1 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
-const dropDownTextSize1 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize1,
-`<label><input type="radio" id="easy" name="size" value="easy" checked>Easy (10x10)</label>`)
-const dropDownRowSize2 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
-const dropDownTextSize2 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize2,
-`<label><input type="radio" id="medium" name="size" value="medium">Medium (15x15)</label>`)
-const dropDownRowSize3 = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
-const dropDownTextSize3 = addHtmlElement('div', 'mine__drop-down__text', dropDownRowSize3,
-  `<label><input type="radio" id="hard" name="size" value="hard">Hard (25x25)</label>`);
+const dropDownRowSize1 = addHtmlElement(
+  "div",
+  "mine__drop-down__row",
+  dropDownMenu
+);
+const dropDownTextSize1 = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRowSize1,
+  `<label><input type="radio" id="easy" name="size" value="easy" checked>Easy (10x10)</label>`
+);
+const dropDownRowSize2 = addHtmlElement(
+  "div",
+  "mine__drop-down__row",
+  dropDownMenu
+);
+const dropDownTextSize2 = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRowSize2,
+  `<label><input type="radio" id="medium" name="size" value="medium">Medium (15x15)</label>`
+);
+const dropDownRowSize3 = addHtmlElement(
+  "div",
+  "mine__drop-down__row",
+  dropDownMenu
+);
+const dropDownTextSize3 = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRowSize3,
+  `<label><input type="radio" id="hard" name="size" value="hard">Hard (25x25)</label>`
+);
 // const size = document.querySelector('gg');
 document.querySelectorAll('input[name="size"]').forEach((e) => {
-  e.addEventListener('change', () => {
-    localStorage.setItem('size', e.value)
+  e.addEventListener("change", () => {
+    localStorage.setItem("size", e.value);
     clearBoard();
     createBoard();
-  })
-})
+  });
+});
 
 const sizeObject = {
   easy: {
-    widthContent: '266px', // mineContent.style.width
-    grid: 'repeat(10, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthContent: "266px", // mineContent.style.width
+    grid: "repeat(10, 25px)", // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
     widthCeil: 10,
     amountMines: 10,
   },
   medium: {
-    widthContent: '390px', // mineContent.style.width
-    grid: 'repeat(15, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthContent: "390px", // mineContent.style.width
+    grid: "repeat(15, 25px)", // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
     widthCeil: 15,
     amountMines: 40,
   },
   hard: {
-    widthContent: '640px', // mineContent.style.width
-    grid: 'repeat(25, 25px)', // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
+    widthContent: "640px", // mineContent.style.width
+    grid: "repeat(25, 25px)", // mineContentInner.style.gridTemplateColumns && mineContentInner.style.gridTemplateRows
     widthCeil: 25,
     amountMines: 99,
   },
-}
+};
 
 // add separator in dropDownMenu after Size
-addHtmlElement('div', 'mine__drop-down__separator', dropDownMenu)
+addHtmlElement("div", "mine__drop-down__separator", dropDownMenu);
 // add last results in dropDownMenu
-const dropDownRowResults = addHtmlElement('div', 'mine__drop-down__row', dropDownMenu);
-const dropDownResults = addHtmlElement('div', 'mine__drop-down__text', dropDownRowResults, 'Last 10 results...');
-
+const dropDownRowResults = addHtmlElement(
+  "div",
+  "mine__drop-down__row",
+  dropDownMenu
+);
+const dropDownResults = addHtmlElement(
+  "div",
+  "mine__drop-down__text",
+  dropDownRowResults,
+  "Last 10 results..."
+);
 
 // add in topBar
-const backOpacity = addHtmlElement('div', 'backOpacity', topBar); 
-const topBarText = addHtmlElement('div', 'mine__top-bar__text', topBar, 'Game settings')
-topBarText.addEventListener('click', () => {
-  dropDown.classList.toggle('hidden');
-  dropDowns.classList.toggle('z-index');
-  backOpacity.classList.toggle('menu-open');
-})
+const backOpacity = addHtmlElement("div", "backOpacity", topBar);
+const topBarText = addHtmlElement(
+  "div",
+  "mine__top-bar__text",
+  topBar,
+  "Game settings"
+);
+topBarText.addEventListener("click", () => {
+  dropDown.classList.toggle("hidden");
+  dropDowns.classList.toggle("z-index");
+  backOpacity.classList.toggle("menu-open");
+});
 
-backOpacity.addEventListener('click', () => {
-  dropDown.classList.toggle('hidden');
-  dropDowns.classList.toggle('z-index');
-  backOpacity.classList.toggle('menu-open');
-})
+backOpacity.addEventListener("click", () => {
+  dropDown.classList.toggle("hidden");
+  dropDowns.classList.toggle("z-index");
+  backOpacity.classList.toggle("menu-open");
+});
 
-function addHtmlElement(element, classElement, insert, innerText = '') {
+function addHtmlElement(element, classElement, insert, innerText = "") {
   const newElement = document.createElement(element);
   newElement.classList.add(classElement);
   newElement.innerHTML = innerText;
-  return insert.appendChild(newElement)
+  return insert.appendChild(newElement);
 }
 
-const mineContent = addHtmlElement('div', 'mine-content', container);
-const scoreBar = addHtmlElement('div', 'mine__score-bar', mineContent);
+const mineContent = addHtmlElement("div", "mine-content", container);
+const scoreBar = addHtmlElement("div", "mine__score-bar", mineContent);
 
 const flagsLine = document.createElement("div");
 flagsLine.innerHTML = `Flags left: <span class='flags-amount'></span>`;
@@ -140,9 +193,9 @@ timer.innerHTML = `${sec % 60} sec`;
 scoreBar.appendChild(timer);
 
 const clicksNumber = document.createElement("div");
-clicksNumber.classList.add('clicks-number');
+clicksNumber.classList.add("clicks-number");
 clicksNumber.innerHTML = `Clicks number: ${stepsNumber}`;
-scoreBar.appendChild(clicksNumber)
+scoreBar.appendChild(clicksNumber);
 
 const mineContentInner = document.createElement("div");
 mineContentInner.classList.add("mine-content__inner");
@@ -159,9 +212,9 @@ function startTimer() {
 }
 
 function createBoard() {
-  sound.checked = localStorage.getItem('sound') === 'true' ? true : false;
+  sound.checked = localStorage.getItem("sound") === "true" ? true : false;
   // check size in localStorage
-  const size = localStorage.getItem('size');
+  const size = localStorage.getItem("size");
   const width = sizeObject[size].widthCeil;
   const minesAmount = sizeObject[size].amountMines;
   flagsAmount.innerHTML = minesAmount;
@@ -200,11 +253,35 @@ function createBoard() {
       if (!isLeftEdge && squares[i - 1].classList.contains("mine")) total++;
       if (!isRightEdge && squares[i + 1].classList.contains("mine")) total++;
       if (i >= width && squares[i - width].classList.contains("mine")) total++;
-      if (i < (width*(width-1)) && squares[i + width].classList.contains("mine")) total++;
-      if (i >= width && !isLeftEdge && squares[i - 1 - width].classList.contains("mine")) total++;
-      if (i >= width && !isRightEdge && squares[i - width + 1].classList.contains("mine")) total++;
-      if (i < (width*(width - 1)) && !isLeftEdge && squares[i - 1 + width].classList.contains("mine")) total++;
-      if (i < (width*(width - 1)) && !isRightEdge && squares[i + 1 + width].classList.contains("mine")) total++;
+      if (
+        i < width * (width - 1) &&
+        squares[i + width].classList.contains("mine")
+      )
+        total++;
+      if (
+        i >= width &&
+        !isLeftEdge &&
+        squares[i - 1 - width].classList.contains("mine")
+      )
+        total++;
+      if (
+        i >= width &&
+        !isRightEdge &&
+        squares[i - width + 1].classList.contains("mine")
+      )
+        total++;
+      if (
+        i < width * (width - 1) &&
+        !isLeftEdge &&
+        squares[i - 1 + width].classList.contains("mine")
+      )
+        total++;
+      if (
+        i < width * (width - 1) &&
+        !isRightEdge &&
+        squares[i + 1 + width].classList.contains("mine")
+      )
+        total++;
       squares[i].setAttribute("data", total);
       squares[i].innerHTML = total; // testing
     }
@@ -212,23 +289,23 @@ function createBoard() {
   // if (sound.checked) new Audio('./asserts/start.wav').play();
 
   // mouse left click in ceil counter
-document.querySelectorAll('.ceil').forEach((e) => e.addEventListener('mousedown', () => {
-  var e = e || window.event;
-  var btnCode;
-  btnCode = e.button
-  if (btnCode === 0) {
-    if (sound.checked) new Audio('./asserts/click.wav').play();
-    stepsNumber++;
-  }
-  if (btnCode === 2) {
-    if (sound.checked) new Audio('./asserts/right-click.mp3').play();
-  }
- }))
+  document.querySelectorAll(".ceil").forEach((e) =>
+    e.addEventListener("mousedown", () => {
+      var e = e || window.event;
+      var btnCode;
+      btnCode = e.button;
+      if (btnCode === 0) {
+        if (sound.checked) new Audio("./asserts/click.wav").play();
+        stepsNumber++;
+      }
+      if (btnCode === 2) {
+        if (sound.checked) new Audio("./asserts/right-click.mp3").play();
+      }
+    })
+  );
 }
 
-  createBoard();
-
-
+createBoard();
 
 function click(square) {
   let currentId = square.id;
@@ -273,7 +350,7 @@ function click(square) {
 }
 
 function addFlag(square) {
-  const size = localStorage.getItem('size');
+  const size = localStorage.getItem("size");
   const minesAmount = sizeObject[size].amountMines;
 
   if (isGameOver) return;
@@ -292,11 +369,14 @@ function addFlag(square) {
 }
 
 function gameOver(square) {
-  if (sound.checked) new Audio('./asserts/lose.wav').play();
+  if (sound.checked) new Audio("./asserts/lose.wav").play();
   clearInterval(time);
   result.innerHTML = "Boom! Game Over! Try one more time...";
   result.classList.add("game-over");
   isGameOver = true;
+  const size = localStorage.getItem("size");
+  resultsArray.push(`Lose level-${size} in ${sec} sec`);
+  localStorage.setItem('Results', resultsArray);
 
   squares.forEach((square) => {
     if (square.classList.contains("mine")) {
@@ -308,7 +388,7 @@ function gameOver(square) {
 }
 
 function checkSquare(square, currentId) {
-  const size = localStorage.getItem('size');
+  const size = localStorage.getItem("size");
   const width = sizeObject[size].widthCeil;
 
   const isLeftEdge = currentId % width === 0; // left column
@@ -329,7 +409,7 @@ function checkSquare(square, currentId) {
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId < width*(width-1)) {
+    if (currentId < width * (width - 1)) {
       const newId = squares[parseInt(currentId) + width].id;
       const newSquare = document.getElementById(newId);
       click(newSquare);
@@ -344,12 +424,12 @@ function checkSquare(square, currentId) {
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId < width*(width-1) && !isLeftEdge) {
-      const newId = squares[+(currentId) - 1 + width].id;
+    if (currentId < width * (width - 1) && !isLeftEdge) {
+      const newId = squares[+currentId - 1 + width].id;
       const newSquare = document.getElementById(newId);
       click(newSquare);
     }
-    if (currentId < width*(width-1) && !isRightEdge) {
+    if (currentId < width * (width - 1) && !isRightEdge) {
       const newId = squares[+currentId + 1 + width].id;
       const newSquare = document.getElementById(newId);
       click(newSquare);
@@ -358,7 +438,7 @@ function checkSquare(square, currentId) {
 }
 
 function checkForWin() {
-  const size = localStorage.getItem('size');
+  const size = localStorage.getItem("size");
   const minesAmount = sizeObject[size].amountMines;
 
   let matches = 0;
@@ -370,20 +450,25 @@ function checkForWin() {
       matches++;
     }
     if (matches === minesAmount) {
-      if (sound.checked) new Audio('./asserts/win.wav').play();
+      if (sound.checked) new Audio("./asserts/win.wav").play();
       result.innerHTML = `Hooray! You found all mines in ${sec} seconds and ${stepsNumber} moves`;
-      result.classList.add('game-win')
+      result.classList.add("game-win");
       clearInterval(time);
       isGameOver = true;
     }
   }
+  if (matches === minesAmount) {
+    resultsArray.push(`Win level-${size} in ${sec} sec and ${stepsNumber} moves`)
+    localStorage.setItem('Results', resultsArray);
+}
+
 }
 
 function clearBoard() {
-  const size = localStorage.getItem('size');
+  const size = localStorage.getItem("size");
   const minesAmount = sizeObject[size].amountMines;
 
-  mineContentInner.innerHTML = '';
+  mineContentInner.innerHTML = "";
   flags = 0;
   clearInterval(time);
   sec = 0;
@@ -391,8 +476,36 @@ function clearBoard() {
   flagsAmount.innerHTML = minesAmount;
   timer.innerHTML = `${sec % 60} sec`;
   clicksNumber.innerHTML = `Clicks number: ${stepsNumber}`;
-  result.innerHTML = '';
-  result.classList.remove('game-over', 'game-win')
+  result.innerHTML = "";
+  result.classList.remove("game-over", "game-win");
   squares = [];
   isGameOver = false;
 }
+function toggleMenu() {
+  dropDown.classList.toggle("hidden");
+  dropDowns.classList.toggle("z-index");
+  backOpacity.classList.toggle("menu-open");
+}
+
+// modal window with results
+const modalOverlay = addHtmlElement("div", "modal-overlay", document.body);
+const modalWindow = addHtmlElement("div", "modal", document.body);
+
+dropDownResults.addEventListener("click", () => {
+  toggleMenu();
+  modalWindow.innerHTML = 'Last 10 results: ';
+  const array = localStorage.getItem('Results').split(',').reverse();
+  const length = array.length > 10 ? 10 : array.length;
+  for (let i = 0; i < length; i++) {
+    const classResult = array[i].includes('Win') ? 'result-win' : 'result-lose';
+    addHtmlElement('li', classResult, modalWindow, `${i+1}. ${array[i]}`);
+  }
+  modalWindow.classList.add("modal--visible");
+  modalOverlay.classList.add("modal-overlay--visible");
+});
+modalOverlay.addEventListener("click", (e) => {
+  if (e.target == modalOverlay) {
+    modalOverlay.classList.remove("modal-overlay--visible");
+    modalWindow.classList.remove("modal--visible");
+  }
+});
