f0c11693262d99734c2740a9362c1e623fef452b
vitali007tut <vitali007@tut.by>
2023-06-10 14:13:59 +0300

    init: add property cogfins

--- News API/webpack.dev.config.js
+++ News API/webpack.dev.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: {
-            directory: path.join(__dirname, '../dist'),
+            directory: path.join(__dirname, './dist'),
         },
     },
 };
