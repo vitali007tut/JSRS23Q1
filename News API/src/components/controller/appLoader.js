eb530799a3a04c2cba098f14fde423c5112236ae
vitali007tut <vitali007@tut.by>
2023-06-10 13:54:14 +0300

    init: copy folder

---
+++ News API/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '87f9980bfcb64b06827bd9f4eb0b1944', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
